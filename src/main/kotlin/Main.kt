7c6bc275383b91192802d5737f917fe67d6412ca
Dirk Jäckel <github@dirk.jaeckel.name>
2025-04-08 12:33:21 +0200

    etch and parse Manifest

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,43 +1,94 @@
-import org.kethereum.model.Address
-import java.io.File
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
-fun main(args: Array<String>) {
-    println("args: $args")
-    val indexParser = IndexParser()
-    indexParser.parse(File("/Users/biafra/trueblocks-kotlin//007799261-007800000_QmQBf3PAoFfUaJZcsCQDj4iSziN56xmyaDiQGM12bTkWdE.index"))
-//   indexParser.parse(File("/Users/biafra/trueblocks-kotlin/020782939-020785534_QmePxCpxtCQSDVcGTbQaXNARQjs1Us2WH6tXvixQEqZjCG.index"))
+data class ManifestResponse(
+    val version: String,
+    val chain: String,
+    val specification: String,
+    val config: Config,
+    val chunks: List<Chunk>
+)
 
-    val bloomFileName = "./007799261-007800000_QmSCAPfekmwUG2UrC8tQzjfh3mRq9ns7515ookmwYuWaUq.bloom"
-//    val bloomFileName = "/Users/biafra/trueblocks-kotlin/QmR5XPnYuJuypCu8LzbWKki3i1DnfWZsgJF9JsqPWL2hCF.bloom"
-//    val bloomFileName = "/Users/biafra/trueblocks-kotlin/020782939-020785534_QmQfn7HkkyjiipBMYvnoQExp7G26NVv17a1pJZyPGpVuf6.bloom"
-    val addressString = "0xfffc3ead0df70e9bbe805af463814c2e6de5ae79"
-//    val addressString = "0x56EE04A8599B0988C86Cdcdf9A815403A99a5639"
-//    val addressString = "0xb6703c0A4c1e856e48fcF3E41C2621e1743a6fEC" //  https://etherscan.io/tx/0xb59a859486bed9baa911d3d61a36c13b2c9a684173047dbb876d99dcb498808e
-//    val bloomParser = BloomParser()
-//    bloomParser.parse(File(bloomFileName))
+data class Config(
+    val appsPerChunk: Int,
+    val snapToGrid: Int,
+    val firstSnap: Int,
+    val unripeDist: Int
+)
 
-    val bloom = Bloom.openBloom(File(bloomFileName), false)
-    val stats = bloom.getStats()
-//    println("stats: $stats")
+data class Chunk(
+    val range: String,
+    val bloomHash: String,
+    val bloomSize: Int,
+    val indexHash: String,
+    val indexSize: Int
+)
 
-   val isMember = bloom.isMemberBytes(Address(addressString))
+/*
+{
+  "version": "trueblocks-core@v0.40.0",
+  "chain": "mainnet",
+  "specification": "QmUou7zX2g2tY58LP1A2GyP5RF9nbJsoxKTp299ah3svgb",
+  "config": {
+    "appsPerChunk": 2000000,
+    "snapToGrid": 100000,
+    "firstSnap": 2300000,
+    "unripeDist": 28
+  },
+  "chunks": [
+    {
+      "range": "000000000-000000000",
+      "bloomHash": "QmYhuaJu9bHAGpSsuaQug7bjnLcoE6B5PCZsg4XZGVFbKy",
+      "bloomSize": 131114,
+      "indexHash": "QmaKUsfH5AXqPJgjGAQFZWRheF1jtc5PqGi8cYrwmMCXdu",
+      "indexSize": 320192
+    },
+    {
+      "range": "000000001-000590510",
+      "bloomHash": "QmTex6bV4wmwnHtLhyMK64smwgtnN6oZPMn26863nGkYW7",
+      "bloomSize": 131114,
+      "indexHash": "QmZ7ByHDQhqe8y8wZoJCk73AGYizG8Nnva7VtAoWGbPdHn",
+      "indexSize": 16821332
+    }
+      ]
+}
+ */
+fun fetchAndParseUrl(url: String): ManifestResponse? {
+    // Create an OkHttpClient instance
+    val client = OkHttpClient()
 
-    println("isMember: $isMember")
-    //val addressChecker = CheckAddress()
-//    addressChecker.main("0xfffeb249dddf766425f3ddc81d033b7100cf4c3b", "/Users/biafra/trueblocks-kotlin/007799261-007800000_QmSCAPfekmwUG2UrC8tQzjfh3mRq9ns7515ookmwYuWaUq.bloom")
-    //addressChecker.main(addressString, bloomFileName)
-//    addressChecker.main("0xffffffffffffffffffffffffee77f8ed97e1ac84", "/Users/biafra/trueblocks-kotlin/020782939-020785534_QmQfn7HkkyjiipBMYvnoQExp7G26NVv17a1pJZyPGpVuf6.bloom")
+    // Build the request
+    val request = Request.Builder()
+        .url(url)
+        .build()
+
+    // Execute the request
+    client.newCall(request).execute().use { response ->
+        if (!response.isSuccessful) {
+            println("Request failed: ${response.code}")
+            return null
+        }
+
+        // Get the response body as a string
+        val responseBody = response.body?.string() ?: return null
+
+        // Create a Moshi instance
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory()) // Add support for Kotlin data classes
+            .build()
+
+        // Create a JSON adapter for the data class
+        val adapter = moshi.adapter(ManifestResponse::class.java)
+
+        // Parse the JSON response
+        return adapter.fromJson(responseBody)
+    }
 }
 
-
-
-//
-//fun main() {
-//    val file = RandomAccessFile("QmdDR7VXHVyomhCGATpGEL6wtFy13CbigwxsVugW8xQUfk.bloom", "r")
-//    val bloom = Bloom(file, headerSize = 128, count = 10u)
-//    val address = Address(ByteArray(20) { 0 }) // Replace with actual address bytes
-//
-//    val isMember = bloom.isMemberBytes(address)
-//    println("Is member: $isMember")
-//}
-//
+fun main() {
+    val url = "https://ipfs.unchainedindex.io/ipfs/QmYjUzLhetTPovBydBoVuzCYnDRABWtfjeBL6JbxopadJG"
+    val result = fetchAndParseUrl(url)
+    println("Parsed result: $result")
+}
